d27f0a8a57e3461e59fe7eaab41a98cdc7e82215
tuoitho <tuoithokhoc1414@gmail.com>
2025-01-05 21:41:59 +0700

    init project (backend)

---
+++ src/main/java/com/careerconnect/CareerConnectApplication.java
@@ -0,0 +1,13 @@
+package com.careerconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/careerconnect/CareerConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
