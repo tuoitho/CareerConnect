513dab9846f35a5b134f9aa753a7c8c2abadbf92
tuoitho <tuoithokhoc1414@gmail.com>
2025-01-05 23:05:49 +0700

    add dotenv-spring

--- src/main/java/com/careerconnect/CareerConnectApplication.java
+++ src/main/java/com/careerconnect/CareerConnectApplication.java
@@ -1,13 +1,31 @@
 package com.careerconnect;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CareerConnectApplication {
 
     public static void main(String[] args) {
+        loadDotenv();
         SpringApplication.run(CareerConnectApplication.class, args);
     }
 
+    public static void loadDotenv() {
+//            System.out.println("DB Driver: " + System.getProperty("DB_DRIVER"));
+//            System.out.println("DB URL: " + System.getProperty("DB_URL"));
+//            System.out.println("DB Username: " + System.getProperty("DB_USERNAME"));
+            Dotenv dotenv = Dotenv.configure().load();
+            System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+            System.setProperty("DB_URL", dotenv.get("DB_URL"));
+            System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+            System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+            // Kiểm tra các giá trị được lấy từ dotenv
+//            System.out.println("DB Driver: " + System.getProperty("DB_DRIVER"));
+//            System.out.println("DB URL: " + System.getProperty("DB_URL"));
+//            System.out.println("DB Username: " + System.getProperty("DB_USERNAME"));
+    }
 }
---
+++ src/main/java/com/careerconnect/config/EnvConfig.java
@@ -0,0 +1,22 @@
+package com.careerconnect.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnvConfig {
+    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    public static String get(String key) {
+        return dotenv.get(key);
+    }
+
+    static String dbUrl=dotenv.get("DB_URL");
+    public static void main(String[] args) {
+        System.out.println("Database URL: " + dbUrl);
+    }
+
+}
