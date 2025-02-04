fabff84f6ba065e08b33e398472e11a6b49d423b
tuoitho <tuoithokhoc1414@gmail.com>
2025-02-04 16:35:01 +0700

    add enum

---
+++ src/main/java/com/careerconnect/enums/IndustryEnum.java
@@ -0,0 +1,19 @@
+package com.careerconnect.enums;
+
+public enum IndustryEnum {
+    TECHNOLOGY,
+    HEALTHCARE,
+    FINANCE,
+    EDUCATION,
+    RETAIL,
+    ENERGY,
+    TRANSPORTATION,
+    TRAVEL,
+    REAL_ESTATE,
+    MANUFACTURING,
+    AGRICULTURE,
+    PROFESSIONAL_SERVICES,
+    MEDIA,
+    FOOD_BEVERAGE,
+    ENVIRONMENT
+}
---
+++ src/main/java/com/careerconnect/enums/JobCategoryEnum.java
@@ -0,0 +1,28 @@
+package com.careerconnect.enums;
+
+public enum JobCategoryEnum {
+    IT,
+    SOFTWARE_DEVELOPMENT,
+    DATA_SCIENCE,
+    MACHINE_LEARNING,
+    WEB_DEVELOPMENT,
+    SALES,
+    MARKETING,
+    ACCOUNTING,
+    GRAPHIC_DESIGN,
+    CONTENT_WRITING,
+    MEDICAL,
+    TEACHING,
+    ENGINEERING,
+    PRODUCTION,
+    LOGISTICS,
+    HOSPITALITY,
+    REAL_ESTATE,
+    LAW,
+    FINANCE,
+    HUMAN_RESOURCES,
+    CUSTOMER_SERVICE,
+    ADMINISTRATION,
+    MANAGEMENT,
+    OTHER
+}
