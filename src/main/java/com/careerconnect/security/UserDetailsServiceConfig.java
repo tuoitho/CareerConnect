9debf7286d3dcf60711835325048a59150ff0b72
tuoitho <tuoithokhoc1414@gmail.com>
2025-03-09 00:13:04 +0700

    update auth

--- src/main/java/com/careerconnect/security/UserDetailsServiceConfig.java
+++ src/main/java/com/careerconnect/security/UserDetailsServiceConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+
+
+//this is a Middleware Bean for solving the problem of circular dependency
 @Configuration
 public class UserDetailsServiceConfig {
 
