ebac5ff55c0fe406dd9fdef97d968b2695e7528f
tuoitho <tuoithokhoc1414@gmail.com>
2025-03-09 00:13:04 +0700

    update auth

--- src/main/java/com/careerconnect/security/TokenBlacklistService.java
+++ src/main/java/com/careerconnect/security/TokenBlacklistService.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,14 +14,18 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class TokenBlacklistService {
     private final RedisTemplate<String, String> redisTemplate;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public void addToBlacklist(String token) {
-        redisTemplate.opsForValue().set(token, "blacklisted", Duration.ofMinutes(30)); // Chặn trong 30 phút
+    public void addRefreshTokenToBlacklist(String token) {
+        redisTemplate.opsForValue().set(token, "blacklisted", Duration.ofDays(30)); // Vi thoi gian song cua refresh token la 30 ngay
+    }
+    public void addAccessTokenToBlacklist(String token) {
+        redisTemplate.opsForValue().set(token, "blacklisted", Duration.ofMinutes(120));
     }
 
     public boolean isBlacklisted(String token) {
 //        return redisTemplate.hasKey(token);
-//        tamj thoi return false de do ton chi phi cloud
+//        tamj thoi return false
         return false;
     }
 
